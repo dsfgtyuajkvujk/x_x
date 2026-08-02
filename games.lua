ba5a368757b1a06c885a828719143ecc6abb5578
dsfgtyuajkvujk <fortune.lua@protonmail.com>
2026-08-01 22:54:09 -0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,5 +1,6 @@
 local Games = {
-  [16472538603] = "https://raw.githubusercontent.com/dsfgtyuajkvujk/x_x/refs/heads/main/folder/thabronx3.lua"
+  [16472538603] = "https://raw.githubusercontent.com/dsfgtyuajkvujk/x_x/refs/heads/main/folder/thabronx3.lua", -- Tha Bronx 3
+  [3851622790] = "", -- 
 }
 
 return Games
