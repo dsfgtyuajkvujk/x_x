d0a680fc2f5fe133648813766551e3f6b8c76c81
dsfgtyuajkvujk <fortune.lua@protonmail.com>
2026-08-01 22:53:15 -0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,27 +1,5 @@
 local Games = {
-  [16472538603] = "https://raw.githubusercontent.com/dsfgtyuajkvujk/x_x/refs/heads/main/folder/thabronx3.lua", -- Tha Bronx 3
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
-  [0] = "", -- 
+  [16472538603] = "https://raw.githubusercontent.com/dsfgtyuajkvujk/x_x/refs/heads/main/folder/thabronx3.lua"
 }
 
 return Games
